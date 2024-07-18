6ab0c25463db64c44a1ebe7717aa3076992c278e
robot_wbw <lin_yang_qi@qq.com>
2024-07-18 13:58:06 +0800

    添加了一点说明，此外也调整了部分“实体”的文件夹位置

---
+++ 辅助/魔方房间思路.mm
@@ -0,0 +1,202 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="魔方房间" FOLDED="false" ID="ID_1978914572" CREATED="1708658296091" MODIFIED="1721280792851">
+<edge STYLE="bezier" COLOR="#dd0000" WIDTH="1" DASH="SOLID"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="93" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#000000" zoom="1.242">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" show_icon_for_attributes="true" associatedTemplateLocation="file:/H:/大学/【学期分类】/大四下/数学物理方法/数学物理方法.mm" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1153876372" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#ffffff" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" CHILD_NODES_LAYOUT="AUTO_CENTERED" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID" MAX_WIDTH="20 cm" MIN_WIDTH="0 cm">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#fefefe" WIDTH="2" TRANSPARENCY="120" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1153876372" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="文泉驿正黑" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge COLOR="#808080"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/html"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/html"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<cloud COLOR="#000033" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#660000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#660000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#00ffcc" STYLE="fork">
+<font NAME="文泉驿正黑" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ff6666" STYLE="fork">
+<font NAME="文泉驿正黑" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#66ff00">
+<font NAME="文泉驿正黑" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+<font NAME="文泉驿正黑" SIZE="14" BOLD="true"/>
+</stylenode>
+<stylenode TEXT="公式" COLOR="#ffcc00" BACKGROUND_COLOR="#333300" STYLE="rectangle" FORMAT="latexPatternFormat">
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/latex"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/latex"/>
+</stylenode>
+<stylenode TEXT="批注" COLOR="#e1dfbb" STYLE="fork">
+<font SIZE="8"/>
+<edge STYLE="bezier" DASH="CLOSE_DOTS"/>
+</stylenode>
+<stylenode TEXT="概念" COLOR="#99ff99" BACKGROUND_COLOR="#003300" STYLE="bubble"/>
+<stylenode TEXT="定理" COLOR="#ff9999" BACKGROUND_COLOR="#330000" STYLE="bubble"/>
+<stylenode TEXT="讨论情况" STYLE="wide_hexagon" TEXT_ALIGN="CENTER"/>
+<stylenode TEXT="关键" COLOR="#ffff33" STYLE="rectangle" BORDER_WIDTH="5 px" BORDER_DASH="DASHES"/>
+<stylenode TEXT="次标题">
+<edge COLOR="#ff0000"/>
+<font NAME="微软雅黑" SIZE="20"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff6666">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffcccc">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#99ff99"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ccffff"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#ff99cc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#ccccff"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#ffffcc"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="简介" POSITION="bottom_or_right" ID="ID_151999098" CREATED="1721280794439" MODIFIED="1721282176968">
+<edge COLOR="#dddd00"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      魔方房间，一个gridmap空间，但是有方式可以让这里的块如魔方一般地“旋转”来改变整体位置。
+    </p>
+    <p>
+      其中还有各种各样的功能块来对魔方旋转作出反应，也可以是魔方内部空间结构导致只有某些状态可以形成通路或垫脚石。
+    </p>
+    <p>
+      可以用于搭建跑酷、密室等各种游戏
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="结构" POSITION="bottom_or_right" ID="ID_1250759922" CREATED="1721280819661" MODIFIED="1721280819868">
+<edge COLOR="#00dd00"/>
+<node TEXT="魔方旋转系统" ID="ID_826072390" CREATED="1721280821709" MODIFIED="1721280829131">
+<node TEXT="旋转功能主体代码" ID="ID_1015521646" CREATED="1721280881640" MODIFIED="1721280917755"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      已基本实现
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="不过目前还没验证对于单元格的旋转是否也做到了" ID="ID_1444091719" CREATED="1721280918541" MODIFIED="1721280936004"/>
+</node>
+<node TEXT="控制方式" ID="ID_446049565" CREATED="1721281068199" MODIFIED="1721281217610"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      两个视角平滑切换
+    </p>
+    <p>
+      （摄像头可能要独立出来，或者改变节点？）
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="简单分离方法：" ID="ID_995231344" CREATED="1721281757242" MODIFIED="1721281768511">
+<node TEXT="玩家第一人称" ID="ID_1477191532" CREATED="1721281080258" MODIFIED="1721281084254">
+<node TEXT="在房间中，内部观察，所见有限" ID="ID_1295147589" CREATED="1721281096258" MODIFIED="1721281120722"/>
+<node TEXT="一种方案是沿视角方向作逆时针旋转魔方最近的层" ID="ID_822574643" CREATED="1721281132779" MODIFIED="1721281164288"/>
+</node>
+<node TEXT="上帝视角第三人称" ID="ID_1118998777" CREATED="1721281085216" MODIFIED="1721281093866">
+<node TEXT="纵观全体" ID="ID_1806085347" CREATED="1721281165983" MODIFIED="1721281180196"/>
+</node>
+</node>
+<node TEXT="模块化方法：“控制台“" ID="ID_848319817" CREATED="1721281769246" MODIFIED="1721281792576">
+<node TEXT="一个控制台可以对多种“交互方式”进行逻辑组合，&#xa;然后决定使用哪些旋转轴与旋转中心、范围等" ID="ID_1997847680" CREATED="1721281782975" MODIFIED="1721281875828">
+<node TEXT="这种方式可以更通用" ID="ID_185395872" CREATED="1721281847312" MODIFIED="1721281893523"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      可以设置功能块用于旋转魔方了
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="功能块" ID="ID_33236088" CREATED="1721280838944" MODIFIED="1721280839584">
+<node TEXT="说明" ID="ID_1060696647" CREATED="1721280944195" MODIFIED="1721280962554">
+<node TEXT="在gridmap中的特殊方块or道具，对于游戏内容或魔方旋转有影响……" ID="ID_60767805" CREATED="1721280957130" MODIFIED="1721281005831"/>
+</node>
+<node TEXT="设计列表/例子" ID="ID_910128639" CREATED="1721280948092" MODIFIED="1721281016215">
+<node TEXT="门" ID="ID_1553194732" CREATED="1721281016829" MODIFIED="1721281019881"/>
+<node TEXT="定向块" ID="ID_1793152681" CREATED="1721281020622" MODIFIED="1721281024637"/>
+<node TEXT="按钮" ID="ID_711949421" CREATED="1721281026196" MODIFIED="1721281029139"/>
+<node TEXT="充电块" ID="ID_1081147008" CREATED="1721281029834" MODIFIED="1721281038347">
+<node TEXT="类似mc的红石块" ID="ID_490784835" CREATED="1721281043800" MODIFIED="1721281050326"/>
+</node>
+<node TEXT="捆绑块" ID="ID_1110430827" CREATED="1721281038863" MODIFIED="1721281042950">
+<node TEXT="限制魔方旋转方式" ID="ID_1930280082" CREATED="1721281052291" MODIFIED="1721281061417"/>
+</node>
+<node TEXT="旋转控制块" ID="ID_1706574992" CREATED="1721281913589" MODIFIED="1721281920368"/>
+</node>
+</node>
+<node TEXT="玩家控制" ID="ID_1684752357" CREATED="1721280842862" MODIFIED="1721280856344">
+<node TEXT="类似mc的方式，右键与功能块进行交互" ID="ID_449787423" CREATED="1721281922371" MODIFIED="1721281979130"/>
+<node TEXT="但上帝视角状态下需要锁住玩家角色本身的控制" ID="ID_175365064" CREATED="1721281980308" MODIFIED="1721281999274"/>
+</node>
+<node TEXT="用户界面（UI）" ID="ID_1179866634" CREATED="1721280849718" MODIFIED="1721280870601">
+<node TEXT="需要什么做什么就好了" ID="ID_1843143172" CREATED="1721282002277" MODIFIED="1721282016169"/>
+</node>
+</node>
+</node>
+</map>
